649866d268c01fec338365a8ad9683bea1d7eec6
Dmitriy <krogal@mail.ru>
2023-09-06 11:59:29 +0500

    Адреса новые

--- data.js
+++ data.js
@@ -1,11 +1,10 @@
 var markser = 
 [
-    {street: "ул. Республики, 51, ТЦ Юность (Магнит)", photos: ['юность.jpg','юность2.jpg'], lat_lng:[63.1945012, 75.4434749], url: "https://vk.com/wall-212956088_5"},
+    {street: "ул. Советская, 43, Магнит", photos: ['юность.jpg','юность2.jpg'], lat_lng:[63.189922, 75.447519], url: "https://vk.com/wall-212956088_5"},
     {street: "ул. Новоселов, 6, ТЦ Городок", photos: [], lat_lng:[63.1863519, 75.4442878], url: "https://vk.com/wall-212956088_6"},
-    {street: "ул. 60 лет СССР, 31, ТЦ Южный (возле банкоматов)", photos: [], lat_lng:[63.1965184, 75.4289322], url: "https://vk.com/wall-212956088_8"},
+    {street: "ул. 60 лет СССР, 31, ТЦ Южный (возле банкоматов)", photos: [], lat_lng:[63.196321, 75.429248], url: "https://vk.com/wall-212956088_8"},
     {street: "ул. Советская, 83, Магнит", photos: [], lat_lng:[63.199187, 75.461037], url: "https://vk.com/wall-212956088_3"},
     {street: "пр-кт Мира, 68а, ТЦ Афганец (Вторник выходной)", photos: [], lat_lng:[63.1994782, 75.4404605], url: "https://vk.com/wall-212956088_1"},
 	{street: "4-й проезд, панель 1/33а (Эко БАЗАР)", photos: [], lat_lng:[63.196718, 75.479142], url: "https://vk.com/wall-212956088_4"},
-	{street: "ул. Ленина, Цветочный ряд (павильён №14)", photos: [], lat_lng:[63.195636, 75.438614], url: "https://vk.com/coffee_tut_nsk"},
-	{street: "ул. Муравленко, 22 (Магнит)", photos: [], lat_lng:[63.190936, 75.413830], url: "https://vk.com/coffee_tut_nsk"},
+	{street: "ул. Муравленко, 22 (Магнит)", photos: [], lat_lng:[63.190928, 75.414319], url: "https://vk.com/coffee_tut_nsk"},
 ]
